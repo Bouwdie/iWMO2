60d7ffd5f07ddbc514f9c407586745c7d5c08d48
Gerben Geijteman <gerben@hyperized.net>
2016-09-20 00:57:44 +0200

    PHP7 is a primary requirement now, add basic tests to Datum and update code to PHP7

--- src/Datatypes/Primitief/Datum.php
+++ src/Datatypes/Primitief/Datum.php
@@ -12,14 +12,28 @@ class Datum
     private $formaat = 'Y-m-d'; // 2016-02-18
     private $waarde = null;
 
-    public function __construct($date) {
-        if(\DateTime::createFromFormat($this->formaat, $date) !== false)
+    public function __construct($waarde) {
+        $this->setWaarde($waarde);
+    }
+
+    public function getWaarde(): string
+    {
+        return $this->waarde;
+    }
+
+    public function setWaarde($waarde)
+    {
+        if(!is_string($waarde))
+        {
+            throw new \Exception('Invoer waarde \''. $this->beschrijving .'\' geen string.');
+        }
+        if(\DateTime::createFromFormat($this->formaat, $waarde) !== false)
         {
             $errors = \DateTime::getLastErrors();
             if (!empty($errors['warning_count'])) {
                 throw new \Exception('Primitieve Datatype \''. $this->beschrijving .'\' met een invalide tijd buiten bereik.');
             }
-            $this->waarde = $date;
+            $this->waarde = $waarde;
         } else {
             throw new \Exception('Primitieve Datatype \''. $this->beschrijving .'\' met een invalide formaat opgegeven.');
         }
--- tests/Datatypes/Primitief/DatumTest.php
+++ tests/Datatypes/Primitief/DatumTest.php
@@ -16,4 +16,27 @@ class DateTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertInstanceOf(Datum::class, $this->object);
     }
+
+    public function testIncorrectRange() {
+        $this->expectException(\Exception::class);
+        $this->object->setWaarde('1700-13-32');
+    }
+
+    public function testCorrectRange() {
+        $this->object->setWaarde('2001-12-31');
+    }
+
+    public function testIncorrectFormat() {
+        $this->expectException(\Exception::class);
+        $this->object->setWaarde('01-01-2012');
+    }
+
+    public function testIncorrectData() {
+        $this->expectException(\Exception::class);
+        $this->object->setWaarde(['hello', 'world', 1337]);
+    }
+
+    public function testGetter() {
+        $this->assertSame($this->object->getWaarde(), date('Y-m-d'));
+    }
 }
