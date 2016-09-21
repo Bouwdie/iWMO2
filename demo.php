165d1f6da99384fe5d41a8c4940d0ea1cc2523bc
Gerben Geijteman <gerben@hyperized.net>
2016-09-21 02:23:42 +0200

    demo first Logisch datatype

--- demo.php
+++ demo.php
@@ -2,6 +2,7 @@
 
 namespace Hyperized\Iwmo2;
 
+use Hyperized\Iwmo2\Datatypes\Logisch\BurgelijkeStaat;
 use Hyperized\Iwmo2\Datatypes\Primitief\Datum;
 use Hyperized\Iwmo2\Datatypes\Primitief\Nummer;
 use Hyperized\Iwmo2\Datatypes\Primitief\Tekst;
@@ -10,9 +11,15 @@ use Hyperized\Iwmo2\Datatypes\Primitief\Tijd;
 require('src/autoload.php');
 
 // Primitieve datatypes
-var_dump(new Datum('2016-02-18'));
-var_dump(new Nummer(15));
-var_dump(new Tekst('Hello World!'));
-var_dump(new Tekst('AB', null, 'NUM061', null, null));
-var_dump(new Tekst('9', null, 'COD366', null, null));
-var_dump(new Tijd('14:23:26'));
+print_r(new Datum('2016-02-18'));
+print_r(new Nummer(15));
+print_r(new Tekst('Hello World!'));
+print_r(new Tekst('AB', null, 'NUM061', 2, null));
+print_r(new Tekst('NL', null, 'COD032', 2, null));
+print_r(new Tijd('14:23:26'));
+
+// Logische datatypes
+$burgelijkeStaat = new BurgelijkeStaat('2');
+print_r($burgelijkeStaat);
+print_r($burgelijkeStaat->getWaarde()->getEnumWaarde()); // Gehuwd
+
---
+++ src/Datatypes/Logisch/BurgelijkeStaat.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Hyperized\Iwmo2\Datatypes\Logisch;
+
+use Hyperized\Iwmo2\Datatypes\Primitief\Tekst;
+use Hyperized\Iwmo2\Generiek\Meta;
+
+/**
+ * Class BurgelijkeStaat
+ * @package Hyperized\Iwmo2\Datatypes\Logisch
+ */
+class BurgelijkeStaat
+{
+    use Meta;
+
+    /**
+     * @var null
+     */
+    private $waarde = null;
+
+    /**
+     * BurgelijkeStaat constructor.
+     * @param string $waarde
+     */
+    public function __construct(string $waarde) {
+        $this->setWaarde($waarde);
+    }
+
+    /**
+     * @return Tekst|null
+     */
+    public function getWaarde(): Tekst
+    {
+        return $this->waarde;
+    }
+
+    /**
+     * @param Tekst|null $waarde
+     */
+    public function setWaarde(string $waarde)
+    {
+        $this->waarde = new Tekst($waarde, null, 'COD366', 1, null);
+    }
+
+}
