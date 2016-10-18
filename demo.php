fb40b06e50481627333411fb727426340df0f774
Gerben Geijteman <gerben@hyperized.net>
2016-10-18 23:00:29 +0200

    Update demo to reflect new datatypes

--- demo.php
+++ demo.php
@@ -2,7 +2,17 @@
 
 namespace Hyperized\Iwmo2;
 
+use Hyperized\Iwmo2\Datatypes\Composiet\Adres;
+use Hyperized\Iwmo2\Datatypes\Composiet\Huis;
+use Hyperized\Iwmo2\Datatypes\Logisch\AanduidingWoonadres;
 use Hyperized\Iwmo2\Datatypes\Logisch\BurgelijkeStaat;
+use Hyperized\Iwmo2\Datatypes\Logisch\Huisletter;
+use Hyperized\Iwmo2\Datatypes\Logisch\Huisnummer;
+use Hyperized\Iwmo2\Datatypes\Logisch\HuisnummerToevoeging;
+use Hyperized\Iwmo2\Datatypes\Logisch\LandCode;
+use Hyperized\Iwmo2\Datatypes\Logisch\Plaatsnaam;
+use Hyperized\Iwmo2\Datatypes\Logisch\Postcode;
+use Hyperized\Iwmo2\Datatypes\Logisch\Straatnaam;
 use Hyperized\Iwmo2\Datatypes\Primitief\Datum;
 use Hyperized\Iwmo2\Datatypes\Primitief\Nummer;
 use Hyperized\Iwmo2\Datatypes\Primitief\Tekst;
@@ -12,15 +22,28 @@ require('autoload.php');
 
 // Primitieve datatypes
 print_r(new Datum('2016-02-18'));
-print_r(new Nummer(15));
+print_r(new Nummer(15, 20, 0));
 print_r(new Tekst('Hello World!'));
-print_r(new Tekst('AB', null, 'NUM061', 2, null));
-print_r(new Tekst('414', null, 'COD002', 3, null));
+print_r(new Tekst('AB', 'NUM061', 2, '[A-Z]{2}'));
 print_r(new Tijd('14:23:26'));
 
 // Logische datatypes
 $burgelijkeStaat = new BurgelijkeStaat('2');
 print_r($burgelijkeStaat);
-print_r($burgelijkeStaat->getWaarde()->getEnumWaarde()); // Gehuwd
+print_r($burgelijkeStaat->getValue()->getEnumValue());
 
+// Composiet datatypes
+$adres = new Adres(
+            new Huis(
+                new Huisnummer('12'),
+                new Huisletter('A'),
+                new HuisnummerToevoeging('APT'),
+                new AanduidingWoonadres('AB')
+            ),
+            new Postcode('1234AB'),
+            new Straatnaam('Kerkstraat'),
+            new Plaatsnaam('Ons dorp'),
+            new LandCode('NL')
+);
+print_r($adres);
 
